b81e552c9e62ebf31834bf6e794871ffc1700c1c
hojoon <ujuj0202@gmail.com>
2021-08-19 18:33:20 +0900

    check realtime

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,38 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Home = () => {
+const Home = ({ userObj }) => {
+    console.log(userObj)
     const [tweet, setTweet] = useState("")
-    const onSubmit = (e) => {
+    const [tweets, setTweets] = useState([])
+
+    useEffect(() => {
+        dbService.collection("tweets").onSnapshot((snapshot) => {
+            // snapshot.forEach(document =>{
+            //     const tweetObject = {
+            //         ...document.data(),
+            //         id: document.id
+            //     }
+            //     setTweets((prev) =>
+            //         [tweetObject, ...prev]
+            //     )
+            // })
+
+            // rendering을 적게 하는 방식
+            const tweetArray = snapshot.docs.map((doc) => ({
+                id: doc.id, ...doc.data()
+            }))
+            setTweets(tweetArray)
+        })
+    }, [])
+    console.log(tweets)
+
+    const onSubmit = async (e) => {
         e.preventDefault();
-        dbService.collection("tweets").add({
-            tweet,
-            createAt: Date.now()
+        await dbService.collection("tweets").add({
+            text: tweet,
+            createAt: Date.now(),
+            creatorId: userObj.uid,
         })
         setTweet("")
     }
@@ -21,6 +46,13 @@ const Home = () => {
                 <input type="text" placeholder="what's on your mind" maxLength={120} value={tweet} onChange={onChange}></input>
                 <input type="submit" value="Tweet" />
             </form>
+            <div>
+                {tweets.map(tweet => (
+                    <div key={tweet.id}>
+                        <h4>{tweet.text}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
